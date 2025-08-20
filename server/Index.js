7ffb558539b869b68495e922d43008ff3acd339f
margalit_tubul <margalittubul@gmail.com>
2025-08-20 14:04:35 +0300

    change index.js

--- server/Index.js
+++ server/Index.js
@@ -15,7 +15,23 @@ const app = express()
 const port = process.env.PORT || 3000;
 connectDB();
 
-app.use(cors())
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://confectionery.onrender.com" 
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
+// app.use(cors())
 app.use(bodyParser.json())
 app.use(express.json());
 
