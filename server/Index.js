3ed54768d7aca5b89967e02ff0bce07edcc1c2e0
margalit_tubul <margalittubul@gmail.com>
2026-01-03 19:53:29 +0200

    update index.js

--- server/Index.js
+++ server/Index.js
@@ -52,7 +52,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-
 app.listen(port, () =>
     console.log(`Example app listening on http://localhost:${port}`)
 )
