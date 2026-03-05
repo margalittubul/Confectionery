bb715b07283b810b3721dd6dfb7229a220f5cdbb
margalit_tubul <margalittubul@gmail.com>
2026-03-05 18:07:47 +0200

    try 4 send mail

--- server/EmailService.js
+++ server/EmailService.js
@@ -1,13 +1,16 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 5000,
-  greetingTimeout: 5000,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 export const sendOrderStatusEmail = async (customerEmail, orderData) => {
