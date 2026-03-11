e0a470fba462ae2f6c881e9542f89f62064fbc80
margalit_tubul <margalittubul@gmail.com>
2026-03-11 23:47:19 +0200

    fix

--- client/src/navigate/navigate2.jsx
+++ client/src/navigate/navigate2.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import "./StyleNavigate.css";
 import { useState, useEffect } from "react";
 import { getCustomerProfile } from "../API/CustomerController";
-import ClubJoin from "./ClubJoin";
 
 export default function Navigate2() {
   const [openSection, setOpenSection] = useState(null);
@@ -37,7 +36,9 @@ export default function Navigate2() {
         <footer>
           <div className="footer-wrapper">
             <div className="footer-image">
-               <ClubJoin />
+              <Link to="/CakeChallenge">
+                <img className="li" src="/img/game.jpg" alt="משחק" />
+              </Link>
             </div>
             <div className="footer-grid">
               <div className="footer-column">
